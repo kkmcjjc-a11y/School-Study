decb6e450fdce667149b0799303265e92ae01618
kkmcjjc-a11y <kkmcjjc@gmail.com>
2026-07-10 15:31:17 +0900

    Add files via upload

---
+++ Test/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSignal = new System.Windows.Forms.Button();
+            this.btnSignal2 = new System.Windows.Forms.Button();
+            this.btnTop = new System.Windows.Forms.Button();
+            this.btnMiddle = new System.Windows.Forms.Button();
+            this.btnBot = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSignal
+            // 
+            this.btnSignal.Location = new System.Drawing.Point(127, 66);
+            this.btnSignal.Name = "btnSignal";
+            this.btnSignal.Size = new System.Drawing.Size(486, 453);
+            this.btnSignal.TabIndex = 0;
+            this.btnSignal.Text = "신호";
+            this.btnSignal.UseVisualStyleBackColor = true;
+            this.btnSignal.Click += new System.EventHandler(this.btnSignal_Click);
+            // 
+            // btnSignal2
+            // 
+            this.btnSignal2.Location = new System.Drawing.Point(127, 659);
+            this.btnSignal2.Name = "btnSignal2";
+            this.btnSignal2.Size = new System.Drawing.Size(486, 453);
+            this.btnSignal2.TabIndex = 4;
+            this.btnSignal2.Text = "신호";
+            this.btnSignal2.UseVisualStyleBackColor = true;
+            this.btnSignal2.Click += new System.EventHandler(this.btnSignal2_Click);
+            // 
+            // btnTop
+            // 
+            this.btnTop.Location = new System.Drawing.Point(1129, 66);
+            this.btnTop.Name = "btnTop";
+            this.btnTop.Size = new System.Drawing.Size(547, 279);
+            this.btnTop.TabIndex = 5;
+            this.btnTop.Text = "1";
+            this.btnTop.UseVisualStyleBackColor = true;
+            // 
+            // btnMiddle
+            // 
+            this.btnMiddle.Location = new System.Drawing.Point(1129, 487);
+            this.btnMiddle.Name = "btnMiddle";
+            this.btnMiddle.Size = new System.Drawing.Size(547, 279);
+            this.btnMiddle.TabIndex = 6;
+            this.btnMiddle.Text = "2";
+            this.btnMiddle.UseVisualStyleBackColor = true;
+            // 
+            // btnBot
+            // 
+            this.btnBot.Location = new System.Drawing.Point(1129, 883);
+            this.btnBot.Name = "btnBot";
+            this.btnBot.Size = new System.Drawing.Size(547, 279);
+            this.btnBot.TabIndex = 7;
+            this.btnBot.Text = "3";
+            this.btnBot.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2054, 1275);
+            this.Controls.Add(this.btnBot);
+            this.Controls.Add(this.btnMiddle);
+            this.Controls.Add(this.btnTop);
+            this.Controls.Add(this.btnSignal2);
+            this.Controls.Add(this.btnSignal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSignal;
+        private System.Windows.Forms.Button btnSignal2;
+        private System.Windows.Forms.Button btnTop;
+        private System.Windows.Forms.Button btnMiddle;
+        private System.Windows.Forms.Button btnBot;
+    }
+}
+
---
+++ Test/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSignal_Click(object sender, EventArgs e)
+        {
+            //Q. top, middle, bot
+            //top.text >, < middle.text 크면 빨간색 작으면 노란색
+            //middle bot 비교하고 같으면 둘 다 초록색 다르면 bot에만 파란색
+
+            //    string topText = (btnTop.Text);
+            //    string middleText = (btnMiddle.Text);
+            //    int convertTopText = Convert.ToInt32(topText);
+            //    int convertMiddleText = Convert.ToInt32(middleText);
+
+            //    if (convertTopText < convertMiddleText)
+            //    {
+            //        btnTop.BackColor = Color.Yellow;
+            //        btnMiddle.BackColor = Color.Red;
+            //    }
+
+            //    else if (convertTopText > convertMiddleText)
+            //    {
+            //        btnTop.BackColor = Color.Red;
+            //        btnMiddle.BackColor = Color.Yellow;
+            //    }
+
+            //    if (btnMiddle.Text == btnBot.Text)
+            //    {
+            //        btnMiddle.BackColor = Color.Green;
+            //        btnBot.BackColor = Color.Green;
+            //    }
+            //    else
+            //    {
+            //        btnBot.BackColor = Color.Blue;
+            //        btnMiddle.BackColor = Color.White;
+            //    }
+
+            //1. 텍스트와 텍스트 비교
+            //숫자로 형 변환
+
+            int a = Convert.ToInt32(btnTop.Text);
+            int b = Convert.ToInt32(btnMiddle.Text);
+            int c = Convert.ToInt32(btnBot.Text);
+
+            //2. 숫자 비교해서 색깔 넣기
+
+            if (a > b)
+            {
+                btnTop.BackColor = Color.Red;
+                btnMiddle.BackColor = Color.Yellow;
+            }
+
+            else if (a < b)
+            {
+                btnTop.BackColor = Color.Yellow;
+                btnMiddle.BackColor = Color.Red;
+            }
+
+            //3. 다음 문제
+
+            if (b == c)
+            {
+                btnMiddle.BackColor = Color.Green;
+                btnBot.BackColor = Color.Green;
+            }
+
+            else if (b != c)
+            {
+                btnBot.BackColor = Color.Blue;
+            }
+        }
+
+        private void btnSignal2_Click(object sender, EventArgs e)
+        {
+            //Q
+            //1.신호를 클릭해서 메세지박스 20번 띄우기
+            //2. btnMiddle.text에 3.14 값을 곱하기 할당해서 표시하기
+
+            for (int i = 0; i < 20; i++)
+            {
+                //신호를 클릭하자마자 2번 문항 실행
+                //if (i == 0)
+                //{
+                //    double a = 3.14;
+                //    double MiddleValue = Convert.ToDouble(btnMiddle.Text);
+                //    double MiddleText = MiddleValue * a;
+                //    btnMiddle.Text = Convert.ToString(MiddleText);
+                //}
+                MessageBox.Show("메세지박스");
+            }
+
+            //메세지박스 20번 띄운 후 2번 문항 실행
+            double a = 3.14;
+            double MiddleValue = Convert.ToDouble(btnMiddle.Text);
+            double MiddleText = MiddleValue * a;
+            btnMiddle.Text = Convert.ToString(MiddleText);
+        }
+    }
+}
